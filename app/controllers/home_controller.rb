f52c4f3d96d6962be685b237dfea3c13f7497089
connorGA <ronnoccap@gmail.com>
2024-07-20 14:31:02 -0700

    update routes, controllers, and views to incorporate add plant capability, dropped and recreated and remigrated db to account for new associations between models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [:search]
+  before_action :authenticate_user!, only: [:search, :add_to_collection]
 
   def index
+    @plants = current_user.plants if user_signed_in?
   end
 
   def search
@@ -11,5 +12,12 @@ class HomeController < ApplicationController
     else
       @plants = []
     end
+  end
+
+  def add_to_collection
+    plant_params = params.require(:plant).permit(:common_name, :latin_name, :image_url, :categories, :watering, :light_ideal, :url)
+    plant = Plant.find_or_create_by(plant_params)
+    current_user.plants << plant unless current_user.plants.include?(plant)
+    redirect_to root_path, notice: 'Plant added to your collection!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get 'home/search', to: 'home#search', as: 'search_home'
+  post 'home/add_to_collection', to: 'home#add_to_collection', as: 'add_to_collection_home_index'
 end
